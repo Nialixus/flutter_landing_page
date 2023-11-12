1ce0f545f56d53f8cc080057dfd692181fa769b1
Nialixus <nialixus@gmail.com>
2023-11-12 14:19:59 +0700

    Predeploy firebase

--- android/app/src/main/kotlin/com/example/flutter_landing_page/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_landing_page
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_landing_page/env/env.dart';
+import 'package:flutter_landing_page/firebase_options.dart';
 import 'package:url_strategy/url_strategy.dart';
 import 'package:dart_fusion/dart_fusion.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setPathUrlStrategy();
+
+  // Remove this if you're not setting up firebase ⬇⬇⬇
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     MaterialApp.router(
       title: 'Flutter Landing Page',
